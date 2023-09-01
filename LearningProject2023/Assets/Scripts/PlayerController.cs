fcb24063bf4fb226971eca995c1d22cd13c10818
Natalie <10840498@my.uvu.edu>
2023-08-31 22:48:28 -0600

    Add Jumping control
    I added jumping into the player controller, with ground and platform detection. It took a bit to figure it out cause I had used it in 2d before, and had to remove all the 2d in the script for it to actually work. I also adjusted the gravity so it was less floaty.

--- LearningProject2023/Assets/Scripts/PlayerController.cs
+++ LearningProject2023/Assets/Scripts/PlayerController.cs
@@ -1,33 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+
+    public float speed = 20.0f;
+    public float jumpForce;
+
+    public float xRange = 13.0f;
+    public GameObject projectilePrefab;
+
+    public bool isOnGround = true;
+    public bool isOnPlatform = true;
+
+    private Rigidbody playerRb;
+    
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
    
-       public float speed = 20.0f;
-   
-       public float xRange = 13.0f;
-       public GameObject projectilePrefab;
-       
-       // Update is called once per frame
-       void Update()
-       {
-           if (transform.position.x < -xRange)
-           {
-               transform.position = new Vector3(-xRange,transform.position.y,transform.position.z);
-           }
-           if (transform.position.x > xRange)
-           {
-               transform.position = new Vector3(xRange,transform.position.y,transform.position.z);
-           }
-   
-           if (Input.GetKeyDown(KeyCode.Space))
-           {
-               Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
-           }
-           horizontalInput = Input.GetAxis("Horizontal");
-           transform.Translate(Vector3.right * (horizontalInput * Time.deltaTime * speed));
-       }
+    void Update()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * (horizontalInput * Time.deltaTime * speed));
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && (isOnGround || isOnPlatform))
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            isOnPlatform = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            isOnPlatform = true;
+        }
+    }
 }
