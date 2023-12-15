d0d6d15abd0ce7c7930a56acf802778e82dee457
Natalie <10840498@my.uvu.edu>
2023-12-15 16:41:15 -0700

    Adding UI to color changing script

---
+++ MayaPythonProject/ColorChangeUI.py
@@ -0,0 +1,74 @@
+import maya.cmds as cmds
+
+def change_override_color(selected_objects, color_index):
+    """
+    Change the override color of the shape nodes for the selected objects.
+
+    Parameters:
+        selected_objects (list): List of selected objects.
+        color_index (int): The color index to set for the shape nodes.
+    """
+    if not selected_objects:
+        cmds.warning("No objects selected. Please select at least one object.")
+        return
+
+    # Iterate over selected objects
+    for obj in selected_objects:
+        # Get the shape nodes of the object
+        shape_nodes = cmds.listRelatives(obj, shapes=True, fullPath=True) or []
+
+        if not shape_nodes:
+            cmds.warning(f"No shape nodes found for {obj}.")
+            continue
+
+        # Change the override color of each shape node
+        for shape_node in shape_nodes:
+            # Check if the shape has an override color attribute
+            if cmds.attributeQuery("overrideColor", node=shape_node, exists=True):
+                # Enable override
+                cmds.setAttr(f"{shape_node}.overrideEnabled", 1)
+                # Set the override color
+                cmds.setAttr(f"{shape_node}.overrideColor", color_index)
+
+                print(f"Override color of {shape_node} set to {color_index}.")
+            else:
+                print(f"{shape_node} does not have an override color attribute.")
+
+def create_ui():
+    """
+    Create a simple UI window with a slider to change the override color.
+    """
+    window_name = "overrideColorChangerUI"
+
+    # Check if the UI window already exists and delete it if it does
+    if cmds.window(window_name, exists=True):
+        cmds.deleteUI(window_name, window=True)
+
+    # Create the UI window
+    cmds.window(window_name, title="Override Color Changer", sizeable=False)
+
+    # Create the main layout
+    cmds.columnLayout(adjustableColumn=True)
+
+    # Create a slider for color selection
+    color_slider = cmds.intSliderGrp(label="Override Color", field=True, min=1, max=31, value=1, fieldMinValue=1, fieldMaxValue=31)
+
+    # Create a button to execute the color change
+    cmds.button(label="Change Color", command=lambda x: change_color_from_slider(color_slider))
+
+    # Show the UI window
+    cmds.showWindow(window_name)
+
+def change_color_from_slider(color_slider):
+    """
+    Callback function to change the override color based on the slider value.
+
+    Parameters:
+        color_slider (str): The name of the color slider control.
+    """
+    color_index = cmds.intSliderGrp(color_slider, query=True, value=True)
+    selected_objects = cmds.ls(selection=True)
+    change_override_color(selected_objects, color_index)
+
+# Call the function to create the UI
+create_ui()
