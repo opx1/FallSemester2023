e7f5bbca7ed65721101bf4fca92ee1b0ff852407
Natalie <10840498@my.uvu.edu>
2023-12-15 13:42:49 -0700

    ColorChange Script to change the shape node color

--- MayaPythonProject/ColorChanger.py
+++ MayaPythonProject/ColorChanger.py
@@ -1,31 +1,32 @@
 import maya.cmds as cmds
 
-
-def change_override_color(selected_objects, override_color):
-    for obj in selected_objects:
-        # Get the shape node of the object
-        shapes = cmds.listRelatives(obj, shapes=True, fullPath=True) or []
-
-        for shape in shapes:
-            # Set the override color for the shape node
-            cmds.setAttr(shape + ".overrideColor", override_color)
-            cmds.setAttr(shape + ".overrideRGBColors", 0)  # Disable RGB color override
-
-
-def main():
-    # Get the currently selected objects
-    selected_objects = cmds.ls(selection=True, long=True)
+def change_override_color(selected_objects, color_index):
 
     if not selected_objects:
         cmds.warning("No objects selected. Please select at least one object.")
         return
 
-    # Set the override color to red (color index 13)
-    override_color = 13
+    for obj in selected_objects:
+        # Get the shape nodes of the object
+        shape_nodes = cmds.listRelatives(obj, shapes=True, fullPath=True) or []
 
-    # Call the function to change the override color
-    change_override_color(selected_objects, override_color)
+        if not shape_nodes:
+            cmds.warning(f"No shape nodes found for {obj}.")
+            continue
 
+        # Change the override color of each shape node
+        for shape_node in shape_nodes:
+            # Check if the shape has an override color attribute
+            if cmds.attributeQuery("overrideColor", node=shape_node, exists=True):
+                # Enable override
+                cmds.setAttr(f"{shape_node}.overrideEnabled", 1)
+                # Set the override color
+                cmds.setAttr(f"{shape_node}.overrideColor", color_index)
 
-if __name__ == "__main__":
-    main()
+                print(f"Override color of {shape_node} set to {color_index}.")
+            else:
+                print(f"{shape_node} does not have an override color attribute.")
+
+# Set color index to 13 (red) for the selected objects
+selected_objects = cmds.ls(selection=True)
+change_override_color(selected_objects, 13)
